9bc5a5c80664b2b4c1c0166e9a3a1f4c61b8adc4
rodrigo <zico.borja162@gmail.com>
2026-06-10 16:19:42 -0300

    feat: security improvments with helmet, winston logger added

--- backend/src/app.js
+++ backend/src/app.js
@@ -17,10 +17,29 @@ import { requireUserAuth } from "./middlewares/userAuth.middleware.js";
 import clinicRoutes from "./routes/clinic.routes.js";
 import reportRoutes from "./routes/report.routes.js";
 import documentRoutes from "./routes/document.routes.js";
+import helmet from "helmet";
+import rateLimit from "express-rate-limit";
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  message: {
+    error: "Muitas requisições realizadas. Tente novamente mais tarde.",
+  },
+});
+
 const app = express();
 
-app.use(cors());
+app.use(helmet());
+
+app.use(
+  cors({
+    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    credentials: true,
+  })
+);
 app.use(express.json());
+app.use(limiter);
 
 app.get("/", (req, res) => {
   res.json({ message: "API rodando com sucesso" });
